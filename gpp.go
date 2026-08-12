7e00151abc55c010585ffed0bfb768ccc9f42e53
Saif <saif.silver@gmail.com>
2026-08-12 17:07:54 +0530

    chore: release v1.11.7

--- gpp.go
+++ gpp.go
@@ -19,7 +19,7 @@ import (
 )
 
 // Version is the current release version of the goplusplus framework.
-const Version = "v1.11.6"
+const Version = "v1.11.7"
 
 // CLIOptions defines configuration for application binary CLI flags.
 type CLIOptions struct {
